f0a5ad715835104ae185841a184beedcead60e26
Inma Caleron <inmacale@gmail.com>
2023-10-17 13:35:38 +0200

    segunda subida de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -14,7 +15,7 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
   ],
   declarations: [
     AppComponent,
-    HeroFormComponent
+    HeroFormComponent,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
